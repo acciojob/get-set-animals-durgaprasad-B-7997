802d5eb7ac0b06f2d66e27c21615a3c1b65fb268
B K R V V Durga Prasad <b2kodandaram@gmail.com>
2025-06-13 11:05:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-// 1. Animal Class
-class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+//complete this code
+class Animal {}
 
-  // Getter for species
-  get species() {
-    return this._species;
-  }
+class Dog extends Animal {}
 
-  // Method to log sound
-  makeSound() {
-    console.log(`The ${this.species} makes a sound`);
-  }
-}
+class Cat extends Animal {}
 
-// 2. Cat Class (inherits from Animal)
-class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
-}
-
-// 3. Dog Class (inherits from Animal)
-class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
-}
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
